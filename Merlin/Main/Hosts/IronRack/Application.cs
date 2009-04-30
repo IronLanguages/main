ff9d384283a33b0972f79395f70c7260c02c7a61
Jimmy Schementi <jschementi@gmail.com>
2009-04-30 03:56:05 -0700

    Spring cleaning (aka merging Justin Rudd's version) (cherry picked from commit 69916ceb04a3f8ecad570fb89549da7ef56d0a6c)

---
+++ Merlin/Main/Hosts/IronRack/Application.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using System.Web;
+using IronRuby.Builtins;
+
+namespace IronRuby.Rack {
+    public class Application {
+
+        private readonly object _app;
+        private readonly string _appRoot;
+        private readonly string _rackVersion;
+
+        public object App {
+            get { return _app; }
+        }
+
+        private const string AppRootOptionName = "AppRoot";
+        private const string RackVersionOptionName = "RackVersion";
+
+        public Application(HttpContext context) {
+            _appRoot = GetRoot(context);
+            _rackVersion = GetRackVersion();
+
+
+            InitRack();
+            _app = Rackup();
+        }
+
+        public RubyArray Call(HttpContext context) {
+           return RubyEngine.ExecuteMethod<RubyArray>(_app, "call", Utils.CreateEnv(context));
+        }
+
+        private void InitRack() {
+            RubyEngine.Init();
+            RubyEngine.Require("rubygems");
+            RubyEngine.Require("rack", _rackVersion);
+            RubyEngine.AddLoadPath(_appRoot);
+        }
+
+        private object Rackup() {
+            var fullPath = RubyEngine.FindFile("config.ru");
+            if(fullPath != null) {
+                var content = File.ReadAllText(fullPath, Encoding.UTF8);
+                return RubyEngine.Execute("Rack::Builder.new { " + content + "}");
+            }
+            return null;
+        }
+
+        private static string GetRackVersion() {
+            return ConfigurationManager.AppSettings[RackVersionOptionName] ?? "=1.0.0";
+        }
+
+        private static string GetRoot(HttpContext context) {
+            var root = ConfigurationManager.AppSettings[AppRootOptionName];
+            if (root == null) {
+                root = context.Request.PhysicalApplicationPath;
+            } else {
+                if (!Path.IsPathRooted(root)) {
+                    root = Path.Combine(context.Request.PhysicalApplicationPath, root);
+                }
+                if (!Directory.Exists(root)) {
+                    throw new ConfigurationErrorsException(String.Format(
+                        "Directory '{0}' specified by '{1}' setting in configuration doesn't exist",
+                        root, AppRootOptionName));
+                }
+            }
+            return root;
+        }
+    }
+}
--- Merlin/Main/Hosts/IronRack/ironrack.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-
-gem 'rack', RACK_VERSION
-require 'rack'
-
-$LOAD_PATH.unshift APP_ROOT
-
-$app = eval "Rack::Builder.new { #{File.read APP_ROOT + '/config.ru'} }"
