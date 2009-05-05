078aaaddd0c5468173ff1880b1c80091bb22dd80
Jimmy Schementi <jschementi@gmail.com>
2009-05-04 23:00:52 -0700

    update sintra test

--- Merlin/Main/Hosts/IronRack/IronRuby.Sinatra.App/test_sinatra.rb
+++ Merlin/Main/Hosts/IronRack/IronRuby.Sinatra.App/test_sinatra.rb
@@ -1,9 +1,6 @@
-gets
 require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  @msg1 = 'Hello'
-  @msg2 = 'World'
-  erb '<%= @msg1 %>, <b><%= @msg2 %></b>'
+  "Hello, <b>World</b> at #{Time.now}"
 end
