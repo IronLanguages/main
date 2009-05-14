9fc1bdcf822c6c5abe6a95e932b7513ddfe272ef
Jimmy Schementi <jschementi@gmail.com>
2009-05-14 15:39:33 -0700

    Log KCODE

--- Merlin/Main/Languages/Ruby/Ruby/Runtime/Globals/SpecialGlobalVariableInfo.cs
+++ Merlin/Main/Languages/Ruby/Ruby/Runtime/Globals/SpecialGlobalVariableInfo.cs
@@ -18,6 +18,7 @@ using Microsoft.Scripting;
 using Microsoft.Scripting.Utils;
 using IronRuby.Builtins;
 using System.Text;
+using System.Diagnostics;
 
 namespace IronRuby.Runtime {
     internal sealed class SpecialGlobalVariableInfo : GlobalVariable {
@@ -117,7 +118,9 @@ namespace IronRuby.Runtime {
                 case GlobalVariableId.KCode:
 #if !SILVERLIGHT
                     if (context.RubyOptions.Compatibility == RubyCompatibility.Ruby18) {
-                        return MutableString.Create(KCoding.GetKCodeName(context.KCode));
+                        var kcode = KCoding.GetKCodeName(context.KCode);
+                        Utils.Log("KCODE set to " + kcode, "KCODE");
+                        return MutableString.Create(kcode);
                     }
 #endif
                     context.ReportWarning("variable $KCODE is no longer effective");
