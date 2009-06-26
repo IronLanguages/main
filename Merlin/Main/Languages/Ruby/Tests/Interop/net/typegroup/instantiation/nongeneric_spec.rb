825b81bb92b3e72ed10647646fa437b9a2a78f74
Jim Deville <jdeville@microsoft.com>
2009-06-26 11:53:56 -0700

    syncing to head of tfs

--- Merlin/Main/Languages/Ruby/Tests/Interop/net/typegroup/instantiation/nongeneric_spec.rb
+++ Merlin/Main/Languages/Ruby/Tests/Interop/net/typegroup/instantiation/nongeneric_spec.rb
@@ -4,6 +4,6 @@ describe "Mixed TypeGroups (with non-generic member)" do
   it "don't cache the class information" do
     c = EmptyTypeGroup.new
     c2 = EmptyTypeGroup1.new
-    c2.should_not be_kind_of EmptyTypeGroup[]
+    c2.should_not be_kind_of EmptyTypeGroup
   end
 end
