864d81d85045615cbd6344517998ab5d4c2c0a64
Jim Deville <jdeville@microsoft.com>
2009-07-27 13:19:05 -0700

    Syncing to head of tfs for 0.9 release

---
+++ Merlin/Main/Languages/Ruby/Tests/Interop/net/IronRuby/clr/flagenumeration/and_spec.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + '/../../../../spec_helper'
+describe "IronRuby::Clr::FlagEnumeration#&" do
+  it "needs to be reviewed for spec completeness"
+end
---
+++ Merlin/Main/Languages/Ruby/Tests/Interop/net/IronRuby/clr/integer/and_spec.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + '/../../../../spec_helper'
+describe "IronRuby::Clr::Integer#&" do
+  it "needs to be reviewed for spec completeness"
+end
