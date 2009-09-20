75d6993150e0bb19a40fa2f67752e45bda9071a3
Jimmy Schementi <jschementi@gmail.com>
2009-09-19 17:05:08 -0700

    make sure test_script_tags has a valid XAP file

--- Merlin/Main/Hosts/SilverLight/Scripts/clean.bat
+++ Merlin/Main/Hosts/SilverLight/Scripts/clean.bat
@@ -4,3 +4,5 @@ del %~dp0dlr.js
 del %~dp0..\Tests\dlr.js
 echo removing chiron.log
 del %~dp0..\Tests\chiron.log
+echo removing dlr folder
+rmdir /S /Q %~dp0..\Tests\dlr
--- Merlin/Main/Hosts/SilverLight/Tests/run.bat
+++ Merlin/Main/Hosts/SilverLight/Tests/run.bat
@@ -2,6 +2,11 @@
 echo Updated dlr.js
 echo --------------
 ruby %~dp0Scripts\gen_dlrjs.rb
+
+echo Making dlr.xap folder
+echo -----------------
+if not exist %~dp0dlr (mkdir %~dp0dlr)
+
 pushd %~dp0
 ruby %~dp0..\Scripts\run_tests.rb
 popd
