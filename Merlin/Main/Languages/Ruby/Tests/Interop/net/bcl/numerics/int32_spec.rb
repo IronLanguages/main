f1c588953d78d5df02556201972de3f6d606131e
Jim Deville <jdeville@microsoft.com>
2009-10-02 15:07:31 -0700

    Basic tests to close CP#1679

--- Merlin/Main/Languages/Ruby/Tests/Interop/net/bcl/numerics/int32_spec.rb
+++ Merlin/Main/Languages/Ruby/Tests/Interop/net/bcl/numerics/int32_spec.rb
@@ -30,4 +30,28 @@ describe "System::Int32" do
     1.to_string.should == "In FixnumMixin#to_string"
   end
 
+  it "doesn't map CLR operators" do
+    begin
+      class Fixnum
+        def c
+          @c
+        end
+        alias_method :compare, :<=>
+
+        def <=>(other)
+          @c = other
+          compare(other)
+        end
+      end
+      a=1
+      a.between?(0,2)
+      a.c.should == 2
+    ensure
+      class Fixnum
+        undef :c
+        alias_method :<=>, :compare
+      end
+    end
+
+  end
 end
