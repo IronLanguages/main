680d5439ba141d5c4b88f2073eeb2d7cda886920
Jim Deville <jdeville@microsoft.com>
2009-10-08 15:00:56 -0700

    fixes dev.ps1.
    set-envvar is added which acts like cmd's set method. If you call it with no
    arguments, then it lists the contents of the $env provider, if you call it with
    an arg of x=foo, then it sets $env:x to foo.

--- Merlin/Main/Languages/Ruby/Scripts/powershell/merlin/merlin.psm1
+++ Merlin/Main/Languages/Ruby/Scripts/powershell/merlin/merlin.psm1
@@ -13,40 +13,52 @@ function insert-path {
   set-content Env:\Path ((get-content Env:\Path) + ";" + $append)
 }
 
+function set-envvar {
+  param([string]$var)
+  if ($var -eq "") {
+    Get-ChildItem env: | Sort-Object name
+  } else {
+    if ($var -match "^(\S*?)\s*=\s*(.*)$") {
+      Set-Item -Force -Path "env:$($matches[1])" -Value $matches[2];
+    } else {
+      Write-Error "ERROR Usage: VAR=VALUE"
+    }
+  }
+}
+
 function initialize-merlin {
   param([string]$path = "C:\vsl\")
 
   initialize-vsvars
 
-  set TERM=
-  set MERLIN_ROOT="$path\Merlin\Main"
-  set MERLIN_EXTERNAL="${env:MERLIN_ROOT}\..\External.LCA_RESTRICTED"
-  set RUBY18_BIN="${env:MERLIN_EXTERNAL}\Languages\Ruby\ruby-1.8.6p368\bin"
-  set RUBY18_EXE="${env:RUBY18_BIN}\ruby.exe"
-  set RUBY19_EXE="${env:MERLIN_EXTERNAL}\Languages\Ruby\1.9.1p0\bin\ruby.exe"
-  set GEM_PATH="${env:MERLIN_EXTERNAL}\Languages\Ruby\ruby-1.8.6p368\lib\ruby\gems\1.8"
+  set-envvar TERM=
+  set-envvar MERLIN_ROOT="$path\Merlin\Main"
+  set-envvar MERLIN_EXTERNAL="${env:MERLIN_ROOT}\..\External.LCA_RESTRICTED"
+  set-envvar RUBY18_BIN="${env:MERLIN_EXTERNAL}\Languages\Ruby\ruby-1.8.6p368\bin"
+  set-envvar RUBY18_EXE="${env:RUBY18_BIN}\ruby.exe"
+  set-envvar RUBY19_EXE="${env:MERLIN_EXTERNAL}\Languages\Ruby\1.9.1p0\bin\ruby.exe"
+  set-envvar GEM_PATH="${env:MERLIN_EXTERNAL}\Languages\Ruby\ruby-1.8.6p368\lib\ruby\gems\1.8"
   $alias = $env:MERLIN_ROOT + '\Scripts\bat\Alias.ps1'
   $alias_internal = $env:MERLIN_ROOT + '\Scripts\bat\AliasInternal.ps1'
   if (test-path $alias) { . $alias }
   if (test-path $alias_internal) { . $alias_internal }
    
   if (Test-Path function:r) {Remove-item -Force function:r}
-  insert-path "${env:RUBY18_BIN}"
-  insert-path "${env:MERLIN_ROOT}\External\Tools"
-  insert-path "${env:MERLIN_ROOT}\Scripts\Bat"
-  insert-path "${env:MERLIN_ROOT}\..\Snap\bin"
-  insert-path "${env:MERLIN_EXTERNAL}\Languages\IronRuby\mspec\mspec\bin"   
-  insert-path "${env:MERLIN_ROOT}\Languages\Ruby\Scripts"
-  insert-path "${env:MERLIN_ROOT}\Languages\Ruby\Scripts\Bin"
+  insert-path $env:RUBY18_BIN
+  insert-path "$env:MERLIN_ROOT\External\Tools"
+  insert-path "$env:MERLIN_ROOT\Scripts\Bat"
+  insert-path "$env:MERLIN_ROOT\..\Snap\bin"
+  insert-path "$env:MERLIN_EXTERNAL\Languages\IronRuby\mspec\mspec\bin"   
+  insert-path "$env:MERLIN_ROOT\Languages\Ruby\Scripts"
+  insert-path "$env:MERLIN_ROOT\Languages\Ruby\Scripts\Bin"
 
-  function global:dbin { cd "${env:MERLIN_ROOT}\Bin\Debug" }
-  function global:rbin { cd "${env:MERLIN_ROOT}\Bin\Release" }
+  function global:dbin { cd "$env:MERLIN_ROOT\Bin\Debug" }
+  function global:rbin { cd "$env:MERLIN_ROOT\Bin\Release" }
   if (Test-Path function:\rbt) { Remove-Item -Force function:\rbt }
-  function global:rbl { cd "${env:MERLIN_ROOT}\Languages\Ruby\IronRuby.Libraries"}
-  function global:rbt { cd "${env:MERLIN_ROOT}\Languages\Ruby\Tests"}
-  function global:script { cd "${env:MERLIN_ROOT}\Scripts\Bat" }
-  function global:root { cd "${env:MERLIN_ROOT}" }
-  function global:ext { cd "${env:MERLIN_EXTERNAL}"}
+  function global:rbt { cd "$env:MERLIN_ROOT\Languages\Ruby\Tests"}
+  function global:script { cd "$env:MERLIN_ROOT\Scripts\Bat" }
+  function global:root { cd "$env:MERLIN_ROOT" }
+  function global:ext { cd "$env:MERLIN_EXTERNAL"}
   function global:d {devenv $args}
   if (Test-Path function:ruby) {Remove-item -Force function:ruby}
   if (Test-Path function:irb) {Remove-item -Force function:irb}
@@ -93,7 +105,6 @@ function exit-pushd {
     Pop-Location  
   }  
 }
-Export-ModuleMember initialize-merlin, initialize-vsvars, write-command,
-exit-pushd
+Export-ModuleMember initialize-merlin, initialize-vsvars, write-command, exit-pushd, set-envvar
 
 
