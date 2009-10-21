fb49e02bc27cbd357bc30994943624c1ea0b0175
Jimmy Schementi <jschementi@gmail.com>
2009-10-21 01:48:09 -0700

    Cleanup IronRuby tutorial scripts

--- Merlin/Main/Languages/Ruby/Samples/Tutorial/clean.bat
+++ Merlin/Main/Languages/Ruby/Samples/Tutorial/clean.bat
@@ -1,3 +1,8 @@
-del %~dp0js\dlr.js
-rmdir /S /Q %~dp0app\Libs
-del %~dp0app\Tutorials\*_tutorial.generated.html
+@echo off
+
+echo Removing dlr.js, app\libs, and generated html files
+del %~dp0js\dlr.js > %~dp0clean.log
+del %~dp0app.xap > %~dp0clean.log
+rmdir /S /Q %~dp0app\Libs > %~dp0clean.log
+del %~dp0app\Tutorials\*_tutorial.generated.html > %~dp0clean.log
+del %~dp0clean.log
