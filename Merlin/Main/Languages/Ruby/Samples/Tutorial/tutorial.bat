3bd3575a68f3f8a6021b26098ec80304f29321e6
Jim Deville <jdeville@microsoft.com>
2010-04-12 19:03:26 -0700

    1.0 sync from TFS
    Signed-off-by: Jim Deville <jdeville@microsoft.com>

--- Merlin/Main/Languages/Ruby/Samples/Tutorial/tutorial.bat
+++ Merlin/Main/Languages/Ruby/Samples/Tutorial/tutorial.bat
@@ -1,17 +1,7 @@
 @echo off
-setlocal ENABLEDELAYEDEXPANSION
-
-set IR_CMD="%~dp0..\..\bin\ir.exe"
-if defined MERLIN_ROOT (
-  REM - This is a dev environment. See http://wiki.github.com/ironruby/ironruby
-  if not EXIST !IR_CMD! set IR_CMD="%MERLIN_ROOT%\bin\Release\ir.exe"
-  if not EXIST !IR_CMD! set IR_CMD="%MERLIN_ROOT%\bin\Debug\ir.exe"
-  if not EXIST !IR_CMD! (
-    echo Could not find ir.exe
-    goto END
-  )
-)
-
-%IR_CMD% %~dp0wpf_tutorial.rb %*
-
+pushd %~dp0
+rake run
+set E=%ERRORLEVEL%
+popd
+exit /B %E%
 :END
