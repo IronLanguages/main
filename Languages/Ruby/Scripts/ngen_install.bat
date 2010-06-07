e21e10aeec87d72f3baffebb2013414b55355c0c
Tomas Matousek <tomas.matousek@microsoft.com>
2010-06-06 22:15:52 -0700

    Syncing to TFS
    TFS Changeset 1849162

--- Languages/Ruby/Scripts/ngen_install.bat
+++ Languages/Ruby/Scripts/ngen_install.bat
@@ -1,4 +1,4 @@
-pushd ..\..\..\Bin\Release
+pushd "..\..\..\Bin\V4 Release"
 ngen install ir.exe
 ngen install Microsoft.Scripting.Core.dll
 ngen install IronRuby.Libraries.dll
--- Languages/Ruby/Scripts/ngen_uninstall.bat
+++ Languages/Ruby/Scripts/ngen_uninstall.bat
@@ -1,4 +1,4 @@
-pushd ..\..\..\Bin\Release
+pushd "..\..\..\Bin\V4 Release"
 ngen uninstall ir.exe
 ngen uninstall Microsoft.Scripting.Core.dll
 ngen uninstall IronRuby.Libraries.dll
