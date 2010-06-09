c172bad58b4e0b3768ac4416d2958afefe72e04c
Dave Fugate <dfugate@microsoft.com>
2010-06-09 08:37:49 -0700

    Syncing to TFS
    TFS Changeset 1857263

---
+++ Scripts/Bat/Dev.bat
@@ -0,0 +1,180 @@
+@echo off
+
+set CURRENT=%CD%
+set BAT=%~dp0
+set TERM=
+set DLR_ROOT=%BAT:~0,-13%
+set DLR_VM=
+set PROGRAM_FILES_32=%ProgramFiles%
+set PROGRAM_FILES_64=%ProgramFiles%
+set PROGRAM_FILES_x86=%ProgramFiles(x86)%
+
+if EXIST "%PROGRAM_FILES_x86%" set PROGRAM_FILES_32=%PROGRAM_FILES_x86%
+if EXIST "%PROGRAM_FILES_x86%" set PROGRAM_FILES_64=%ProgramW6432%
+
+REM -- Nullify the existing environment
+call %DLR_ROOT%\Test\Scripts\SetTestEnv.bat
+
+REM -- IronRuby environment variables --
+set RUBY18_BIN=%DLR_ROOT%\External.LCA_RESTRICTED\Languages\Ruby\ruby-1.8.6p368\bin
+set RUBY18_EXE=%RUBY18_BIN%\ruby.exe
+set RUBY19_EXE=%DLR_ROOT%\External.LCA_RESTRICTED\Languages\Ruby\ruby-1.9.1p129\bin\ruby.exe
+set RUBYOPT=
+set GEM_PATH=%DLR_ROOT%\External.LCA_RESTRICTED\Languages\Ruby\ruby-1.8.6p368\lib\ruby\gems\1.8
+
+REM -- IronPython environment variables
+set IRONPYTHONPATH=%DLR_ROOT%\External.LCA_RESTRICTED\Languages\IronPython\26\Lib
+
+REM -- Python environment variables
+set PYTHONPATH=.;%DLR_ROOT%\External.LCA_RESTRICTED\Languages\IronPython\26\lib;%DLR_ROOT%\Languages\IronPython\IronPython\Lib
+
+
+if EXIST "%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\bin\sn.exe" (
+  set SN_UTIL="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\bin\sn.exe"
+  goto SnDone
+)
+
+if EXIST "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\sn.exe" (
+  set SN_UTIL="%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\sn.exe"
+  goto SnDone
+)
+
+if EXIST "%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v7.0A\bin\sn.exe" (
+  set SN_UTIL="%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v7.0A\bin\sn.exe"
+  goto SnDone
+)
+
+if EXIST "%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v6.0A\bin\sn.exe" (
+  set SN_UTIL="%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v6.0A\bin\sn.exe"
+  goto SnDone
+)
+
+if EXIST "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\x64\sn.exe" (
+  set SN_UTIL="%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\x64\sn.exe"
+  goto SnDone
+)
+
+if EXIST "%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v6.0A\bin\x64\sn.exe" (
+  set SN_UTIL="%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v6.0A\bin\x64\sn.exe"
+  goto SnDone
+)
+
+if EXIST "%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\bin\NETFX 4.0 Tools\sn.exe" (
+  set SN_UTIL="%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\bin\NETFX 4.0 Tools\sn.exe"
+  goto SnDone
+)
+
+if EXIST "%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v7.0A\bin\NETFX 4.0 Tools\sn.exe" (
+  set SN_UTIL="%PROGRAM_FILES_64%\Microsoft SDKs\Windows\v7.0A\bin\NETFX 4.0 Tools\sn.exe"
+  goto SnDone
+)
+
+REM SN_UTIL should be defined even if we can't find sn.exe.
+set SN_UTIL="sn.exe"
+
+:SnDone
+
+if exist "%PROGRAM_FILES_32%\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat" (
+    call "%PROGRAM_FILES_32%\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
+    goto EnvDone
+)
+
+if exist "%PROGRAM_FILES_32%\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat" (
+    call "%PROGRAM_FILES_32%\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
+    goto EnvDone
+)
+
+if exist "%PROGRAM_FILES_32%\Microsoft Visual Studio 8.0\SDK\v2.0\Bin\sdkvars.bat" (
+    call "%PROGRAM_FILES_32%\Microsoft Visual Studio 8.0\SDK\v2.0\Bin\sdkvars.bat"
+    goto EnvDone
+)
+
+REM But perhaps only the 64-bit SDK is installed
+if exist "%PROGRAM_FILES_32%\Microsoft.NET\SDK\v2.0 64bit\Bin\sdkvars.bat" (
+    call "%PROGRAM_FILES_32%\Microsoft.NET\SDK\v2.0 64bit\Bin\sdkvars.bat"
+    goto EnvDone
+)
+
+if exist "%PROGRAM_FILES_32%\Microsoft.NET\SDK\v2.0\Bin\sdkvars.bat" (
+    call "%PROGRAM_FILES_32%\Microsoft.NET\SDK\v2.0\Bin\sdkvars.bat"
+    goto EnvDone
+)
+
+:EnvDone
+
+set PATH=%PATH%;%DLR_ROOT%\External\Tools;%DLR_ROOT%\Scripts\Bat;%DLR_ROOT%\Util\Internal\Snap\bin
+set PATH=%PATH%;%DLR_ROOT%\Languages\Ruby\Scripts;%DLR_ROOT%\Languages\Ruby\Scripts\bin;%DLR_ROOT%\External.LCA_RESTRICTED\Languages\IronRuby\mspec\mspec\bin;%RUBY18_BIN%
+set PATH=%PATH%;%DLR_ROOT%\Util\tfpt
+
+REM -- Mono
+if "%1" == "mono" set DLR_VM=%DLR_ROOT%\External.LCA_RESTRICTED\Mono\bin\mono.exe
+if "%1" == "mono" set PATH=%DLR_ROOT%\External.LCA_RESTRICTED\Mono\bin;%PATH%
+
+if not DEFINED HOME_FOR_MSPECRC (
+  if DEFINED HOME (
+      set HOME_FOR_MSPECRC=%HOME%
+      goto SetRubyEnv
+  )
+  
+  if DEFINED HOMEDRIVE (
+    if DEFINED HOMEPATH (
+      set HOME_FOR_MSPECRC=%HOMEDRIVE%%HOMEPATH%
+      goto SetRubyEnv
+    )
+  )
+  if not DEFINED USERPROFILE (
+    echo Error: One of HOME, HOMEDRIVE,HOMEPATH, or USERPROFILE needs to be set
+    goto END
+  )
+  set HOME_FOR_MSPECRC=%USERPROFILE%
+)
+
+:SetRubyEnv
+
+if NOT EXIST "%HOME_FOR_MSPECRC%\.mspecrc" (
+  copy "%DLR_ROOT%\Languages\Ruby\default.mspec" "%HOME_FOR_MSPECRC%\.mspecrc"
+)
+
+doskey /macrofile=%BAT%Alias.txt
+doskey /macrofile=%BAT%AliasInternal.txt
+cd /D %CURRENT%
+
+REM Disable strong name validation for the assemblies we build, if it isn't already
+%SN_UTIL% -Vl | find "*,31bf3856ad364e35" > NUL 2>&1
+IF NOT "%ERRORLEVEL%"=="0" goto DisableSNValidation
+goto Continue
+
+:DisableSNValidation
+
+%SN_UTIL% -Vr *,31bf3856ad364e35
+IF NOT "%ERRORLEVEL%"=="0" goto SnError
+
+:Continue
+
+REM Disable strong name verification for development builds of Silverlight
+%DLR_ROOT%\Util\Internal\Silverlight\x86ret\snskipverf.exe
+
+REM Run user specific setup
+if EXIST %DLR_ROOT%\..\Users\%USERNAME%\Dev.bat call %DLR_ROOT%\..\Users\%USERNAME%\Dev.bat
+
+cls
+
+goto End
+
+:SnError
+
+cls
+color 0C
+echo. 
+echo Disabling strong name validation failed!
+echo.
+echo We use delay signing for assemblies we build and so you need to disable 
+echo strong name validation during development.
+echo This requires elevated permissions. 
+echo Please run this script ONCE using "Run as administrator" command.
+echo.
+
+:End
+
+set BAT=
+set CURRENT=
