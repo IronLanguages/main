ea95073786148f61849a585e8d1bed4a1880d552
Alistair Bush (ali_bush) <ali_bush@gentoo.org>
2010-11-05 22:16:57 +1300

    Add build.sh to show examples of building with xbuild.

---
+++ Runtime/Microsoft.Dynamic/build.sh
@@ -0,0 +1,1 @@
+xbuild /property:TreatWarningsAsErrors=false /property:Configuration=Debug /property:Platform=AnyCPU /property:SolutionDir="..\\..\\Solutions\\" /property:ProjectDir=".\\" Microsoft.Dynamic.csproj
---
+++ Runtime/Microsoft.Scripting.Core/build.sh
@@ -0,0 +1,1 @@
+xbuild /property:TreatWarningsAsErrors=false /property:Configuration=v2Debug /property:Platform=AnyCPU /property:SolutionDir="..\\..\\Solutions\\" Microsoft.Scripting.Core.csproj
