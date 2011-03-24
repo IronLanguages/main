53ec8a160a8b82701ddbe30d0f57ba26bd08efc0
Jeff Hardy <jdhardy@gmail.com>
2011-03-23 21:53:42 -0600

    Make _csv compatible with .NET 2.
    The StringBuilder.Clear() method is new in .NET 4, so add an extension method when building for .NET 2.

--- Languages/IronPython/IronPython.Modules/_csv.cs
+++ Languages/IronPython/IronPython.Modules/_csv.cs
@@ -1199,4 +1199,15 @@ elements will be converted to string.")]
                 PythonExceptions.StandardError, dict, "Error", "_csv");
         }
     }
+
+#if CLR2
+    static class StringBuilderExtensions
+    {
+        internal static StringBuilder Clear(this StringBuilder sb) 
+        {
+            sb.Length = 0;
+            return sb;
+        }
+    }
+#endif
 }
