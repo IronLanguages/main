53cc2ee1e69e618052fd8c5003995a807562cb9f
Jeff Hardy <jdhardy@gmail.com>
2011-04-10 09:28:40 -0600

    Disable unicodedata and \N escapes on Silverlight because it doesn't have System.IO.Compression. (cherry picked from commit 306cf8103d0ae6c75e8521d79bb97bd3f1d90c97)

--- Languages/IronPython/IronPython/Modules/unicodedata.cs
+++ Languages/IronPython/IronPython/Modules/unicodedata.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/* ****************************************************************************
+ *
+ * Copyright (c) Jeff Hardy 2011.
+ *
+ * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+ * copy of the license can be found in the License.html file at the root of this distribution. If 
+ * you cannot locate the  Apache License, Version 2.0, please send an email to 
+ * ironpy@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ * by the terms of the Apache License, Version 2.0.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ *
+ * ***************************************************************************/
+
+#if !SILVERLIGHT
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -330,3 +347,5 @@ namespace IronPython.Modules
         internal readonly int Last;
     }
 }
+
+#endif
