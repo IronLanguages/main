11e728f9a14b47f0e5562f78d49cae98ebf42039
Tomas Matousek <tomas_matousek@hotmail.com>
2011-05-22 13:43:08 -0700

    Fixes Symbol with String comparison (bug 6006)

--- Languages/Ruby/Tests/mspec/rubyspec/command_line/dash_upper_s_spec.rb
+++ Languages/Ruby/Tests/mspec/rubyspec/command_line/dash_upper_s_spec.rb
@@ -1,17 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "The -S command line option" do
-<<<<<<< HEAD
-  it 'finds the file on the path' do
-    this_dir = File.expand_path(File.dirname(__FILE__))
-    fixture_dir = File.expand_path(this_dir + '/../fixtures')
-
-    ENV['PATH'] = "#{fixture_dir};#{ENV['PATH']}"
-    dash_s = ruby_exe(nil, :options => '-S file.rb')
-
-    ENV['PATH'] = ENV['PATH'].split(';')[1..-1].join(';')
-=======
-
   def push_path(dir)
     ENV['PATH'] = "#{dir};#{ENV['PATH']}"
   end
@@ -34,21 +23,7 @@ describe "The -S command line option" do
     with_path(fixture_dir) do
       dash_s = ruby_exe(nil, :options => '-S file.rb')
     end
->>>>>>> 51920c25aaf2a24e82055e0807f51a19a01d3a40
-    expected = ruby_exe("#{fixture_dir}/file.rb")
-    failed_dash_s = ruby_exe(nil, :args => "-S #{this_dir}/file.rb 2>&1")
-
-    dash_s.should == expected
-    dash_s.should_not == failed_dash_s
   end
-
-  it 'does not find the file on the path' do
-    dash_s = ruby_exe(nil, :args => '-S does/not/exist.rb 2>&1')
-    normal = ruby_exe(nil, :args => 'does/not/exist.rb 2>&1')
-    dash_s.should == normal
-  end
-<<<<<<< HEAD
-=======
 
   it 'finds the first file on the path' do
     this_dir = File.expand_path(File.dirname(__FILE__))
@@ -70,5 +45,4 @@ describe "The -S command line option" do
       actual.should == File.expand_path(expected[i] + '/file.rb')
     end
   end
->>>>>>> 51920c25aaf2a24e82055e0807f51a19a01d3a40
 end
