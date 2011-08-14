d2db54221325fe3d770a170f7de3a239234a8bac
Enrico Sada <enrico.sada@gmail.com>
2011-08-14 12:53:40 +0200

    updated rubyspec 'core/range' to latest version (commit rubyspec/rubyspec@ea0ae038fd0e3ab995c44657dbb3f22ef7fa450d )

--- Languages/Ruby/Tests/mspec/rubyspec/core/range/each_spec.rb
+++ Languages/Ruby/Tests/mspec/rubyspec/core/range/each_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "Range#each" do
   it "passes each element to the given block by using #succ" do
@@ -41,13 +41,14 @@ describe "Range#each" do
   end
 
   it "returns self" do
-    (1..10).each {}.should == (1..10)
+    range = 1..10
+    range.each{}.should equal(range)
   end
 
   ruby_version_is "1.8.7" do
     it "returns an enumerator when no block given" do
       enum = (1..3).each
-      enum.should be_kind_of(enumerator_class)
+      enum.should be_an_instance_of(enumerator_class)
       enum.to_a.should == [1, 2, 3]
     end
   end
