1f42bc068818404992e9a4f37a471d17022dd7a5
Enrico Sada <enrico.sada@gmail.com>
2011-08-15 04:02:59 +0200

    updated rubyspec 'library/timeout' to latest version  (commit rubyspec/rubyspec@ea0ae038fd0e3ab995c44657dbb3f22ef7fa450d )

--- Languages/Ruby/Tests/mspec/rubyspec/library/timeout/timeout_spec.rb
+++ Languages/Ruby/Tests/mspec/rubyspec/library/timeout/timeout_spec.rb
@@ -1,10 +1,10 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require File.expand_path('../../../spec_helper', __FILE__)
 require 'timeout'
 
 describe "Timeout.timeout" do
   it "raises Timeout::Error when it times out with no specified error type" do
     lambda {
-      Timeout::timeout(1) do
+      Timeout.timeout(1) do
         sleep 3
       end
     }.should raise_error(Timeout::Error)
@@ -12,25 +12,23 @@ describe "Timeout.timeout" do
 
   it "raises specified error type when it times out" do
     lambda do
-      Timeout.timeout(0.1, StandardError) do
-        sleep 1
+      Timeout.timeout(1, StandardError) do
+        sleep 3
       end
     end.should raise_error(StandardError)
   end
-  
+
   it "does not wait too long" do
     before_time = Time.now
-    begin
-      Timeout::timeout(1) do
+    lambda do
+      Timeout.timeout(1, StandardError) do
         sleep 3
       end
-    rescue Timeout::Error
-      (Time.now - before_time).should < 1.2
-    else
-      flunk
-    end
-  end
+    end.should raise_error(StandardError)
 
+    Time.now.to_f.should be_close(before_time.to_f, 1.2)
+  end
+  
   it "does not return too quickly" do
     before_time = Time.now
     begin
@@ -45,7 +43,7 @@ describe "Timeout.timeout" do
   end
 
   it "returns back the last value in the block" do
-    Timeout::timeout(1) do
+    Timeout.timeout(1) do
       42
     end.should == 42
   end
