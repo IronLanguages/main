82d377030bfe8afd91120086aa1629544fbed796
Jeff Hardy <jdhardy@gmail.com>
2012-02-12 02:01:49 -0800

    Automatically reference IronPython.SQLite.dll when importing sqlite3.

--- External.LCA_RESTRICTED/Languages/IronPython/27/Lib/sqlite3/__init__.py
+++ External.LCA_RESTRICTED/Languages/IronPython/27/Lib/sqlite3/__init__.py
@@ -21,4 +21,12 @@
 #    misrepresented as being the original software.
 # 3. This notice may not be removed or altered from any source distribution.
 
+def _():
+    import sys
+    if sys.platform == 'cli':
+        import clr
+        clr.AddReference('IronPython.SQLite')
+_()
+del _
+
 from dbapi2 import *
