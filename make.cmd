6b7d7c92f4116ec78724e83f11d4cc8dca8b5cb2
Jeff Hardy <jdhardy@gmail.com>
2012-04-28 23:46:01 -0700

    Add "make" script for easy builds on Windows machines.

---
+++ make.cmd
@@ -0,0 +1,73 @@
+@echo off
+setlocal
+set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319;%WINDIR%\Microsoft.NET\Framework\v3.5
+
+:getopts
+if "%1"=="" (goto :default) else (goto :%1)
+goto :exit
+
+:default
+goto :debug
+
+:debug
+set _target=Build
+set _flavour=Debug
+goto :main
+
+:clean-debug
+set _target=Clean
+set _flavour=Debug
+goto :main
+
+:stage-debug
+set _target=Stage
+set _flavour=Debug
+goto :main
+
+:release
+set _target=Build
+set _flavour=Release
+goto :main
+
+:clean-release
+set _target=Clean
+set _flavour=Release
+goto :main
+
+:stage-release
+set _target=Stage
+set _flavour=Release
+goto :main
+
+:package-release
+set _target=Package
+set _flavour=Release
+goto :main
+
+:clean
+echo No target 'clean'. Try 'clean-debug' or 'clean-release'.
+goto :exit
+
+:stage
+echo No target 'stage'. Try 'stage-debug' or 'stage-release'.
+goto :exit
+
+:package
+echo No target 'package'. Try 'package-release'.
+goto :exit
+
+:test
+echo Not Ready
+goto :exit
+
+:distclean
+msbuild /t:DistClean /p:BaseConfiguration=Release /verbosity:minimal /nologo
+msbuild /t:DistClean /p:BaseConfiguration=Debug /verbosity:minimal /nologo
+goto :main
+
+:main
+msbuild /t:%_target% /p:BaseConfiguration=%_flavour% /verbosity:minimal /nologo
+goto :exit
+
+:exit
+endlocal
