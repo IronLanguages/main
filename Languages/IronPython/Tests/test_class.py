c3d1f76667b51b4661435896a87e90a5989fa190
Pawel Jasinski <pawel.jasinski@gmail.com>
2012-12-31 11:12:48 +0100

    added test for cp33622
    (cherry picked from commit 9224002fff9a436f47d0c20fb44637b80aa8a965)

--- Languages/IronPython/Tests/test_class.py
+++ Languages/IronPython/Tests/test_class.py
@@ -3722,6 +3722,7 @@ def test_cp33622():
     AreEqual(object.__repr__ in (None,), False)
     AreEqual(object.__repr__ in (None,object.__repr__), True)
     AreEqual(object.__repr__ in (None,object.__cmp__), False)
+    AreEqual(object.__repr__ in (None,object.__str__), False)
 
 #--MAIN------------------------------------------------------------------------
 run_test(__name__)
