63820e7414e0bd45cfb37d3ea52568560e95e010
slozier <slozier@users.noreply.github.com>
2016-09-17 11:30:15 -0400

    Enable more tests (#1418)
    * Add missing tests and remove duplicate
    
    * Enable sbs tests in release

--- Test/scripts/_runip_sbs.bat
+++ Test/scripts/_runip_sbs.bat
@@ -1,5 +1,5 @@
 ::
 :: side by side test invocation
 ::
-%DLR_ROOT%/External.LCA_RESTRICTED/Languages/CPython/27/python.exe runsbs.py %2
-%1 runsbs.py %2
+%DLR_ROOT%/External.LCA_RESTRICTED/Languages/CPython/27/python.exe runsbs.py %1
+%DLR_ROOT%/Languages/IronPython/Internal/ipy.bat runsbs.py %1
