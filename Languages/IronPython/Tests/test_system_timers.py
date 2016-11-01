fb0fcd9fb68a75f0154994a608d931d602c01d04
slozier <slozier@users.noreply.github.com>
2016-11-01 14:56:20 -0400

    Reduce test_system_timers execution time (#1520)

--- Languages/IronPython/Tests/test_system_timers.py
+++ Languages/IronPython/Tests/test_system_timers.py
@@ -57,11 +57,11 @@ def timer_helper(num_handlers=1, sleep_time=5, event_handlers=[], aTimer=None, e
         for i in xrange(num_handlers): aTimer.Elapsed += System.Timers.ElapsedEventHandler(onTimedEvent)
         for handler in event_handlers: aTimer.Elapsed += System.Timers.ElapsedEventHandler(handler)
     
-        aTimer.Interval = 1000
+        aTimer.Interval = 100
         aTimer.Enabled = True
-        sleep(sleep_time)
+        sleep(sleep_time / 10.0)
         aTimer.Enabled = False
-        sleep(sleep_time)  #give the other thread calling 'onTimedEvent' a chance to catch up
+        sleep(sleep_time / 10.0)  #give the other thread calling 'onTimedEvent' a chance to catch up
         
         #ensure the timer invoked our event handler a reasonable number of times
         Assert(COUNT >= int(sleep_time - sleep_time*error_margin) * (num_handlers+len(event_handlers)), str(COUNT))
@@ -91,7 +91,7 @@ def test_sanity_from_thread():
     print "Waiting for test_sanity to finish",
     while not TIMER_HELPER_FINISHED:
         print ".",
-        sleep(1)
+        sleep(0.1)
 
 
 def test_multiple_events_one_timer():
@@ -129,11 +129,11 @@ def test_elapsed_event_args():
     #create and run the timer
     aTimer = System.Timers.Timer()
     aTimer.Elapsed += System.Timers.ElapsedEventHandler(checkEventArgs)
-    aTimer.Interval = 1000
+    aTimer.Interval = 100
     aTimer.Enabled = True
-    sleep(sleep_time)
+    sleep(sleep_time / 10.0)
     aTimer.Enabled = False
-    sleep(sleep_time)  #give the other thread calling 'onTimedEvent' a chance to catch up
+    sleep(sleep_time / 10.0)  #give the other thread calling 'onTimedEvent' a chance to catch up
     
     #make sure it was called at least once
     Assert(COUNT > 0)
